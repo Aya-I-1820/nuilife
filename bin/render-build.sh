da640273dcd657a9c49af185798510b22e66e642
Aya-I-1820 <216883077+Aya-I-1820@users.noreply.github.com>
2025-10-02 13:00:06 +0900

    add render-build.sh (staging)

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,9 @@ set -o errexit
 
 apt-get update && apt-get install -y libpq-dev
 bundle install
+bin/rails assets:precompile
+bin/rails assets:clean
+
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
 bundle exec rails db:seed
